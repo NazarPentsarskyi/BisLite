e1091ba7c9aa85299814c956308e7acf4df62b2a
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2022-07-16 22:08:12 +0300

    all without footer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ $(function(){
   $('.works__inner').slick({
       //arrows:false,
       slidesToShow: 4,
-      slidesToScroll: 2
+      slidesToScroll: 2,
+      prevArrow: '<button type="button" class="slick-prev"><img src="images/slider_works/slide-left.svg" alt=""></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="images/slider_works/slide-right.svg" alt=""></button>'
   });
 
 });
